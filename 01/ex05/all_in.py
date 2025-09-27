9a9ff68d254c8d24b76d879cbb951fb824c9af23
JosepharDev <josephardev@gmail.com>
2025-09-27 18:15:42 +0100

    add d01/ex05

--- 01/ex05/all_in.py
+++ 01/ex05/all_in.py
@@ -8,8 +8,23 @@ def dict_get_key_from_value(dct, value):
             return key
     return None
 
+def dict_get_value_from_key(dct, key):
+    for item, value in dct.items():
+        if item.upper() == key.upper():
+            return value
+    return None
+
+def clean_dict(dct):
+    tmp = []
+    for item in dct:
+        item = item.strip()
+        if item == "":
+            continue
+        tmp.append(item)
+    return tmp
+
+
 def all_in(names):
-    print(names)
     states = {
         "Oregon" : "OR",
         "Alabama" : "AL",
@@ -22,7 +37,17 @@ def all_in(names):
         "NJ": "Trenton",
         "CO": "Denver"
         }
-    
+    items = clean_dict(names)
+    for item in items:
+        key = dict_get_key_from_value(capital_cities, item)
+        value = dict_get_value_from_key(states, item)
+        if value:
+            print(capital_cities.get(value),"is the state of", dict_get_key_from_value(states, value))
+        elif key:
+            print(capital_cities.get(key), "is the capital of", dict_get_key_from_value(states, key))
+        else:
+            print(item, "is neither a capital city nor a state")
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
