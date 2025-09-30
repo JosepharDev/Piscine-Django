8d585822c423de557f5dbd77db632084169f24f1
JosepharDev <josephardev@gmail.com>
2025-09-30 14:22:29 +0100

    fix

---
+++ 02/ex00/render.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import sys, os, re, settings
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
---
+++ 02/ex00/settings.py
@@ -0,0 +1,46 @@
+Django - 0 - Initiation
+Django - 0 - Starting
+Django - 0 - Oob  = 1500
+Django - 1 - Lib
+Django - 1 - Base Django = 3475
+Django - 2 - SQL = 1000
+Django - 3 - Sessions
+Django - 3 - Advanced
+Django - 3 - Final = 3475
+
+################################
+python for data science
+
+Python - 0 - Starting = 545 / done
+Python - 1 - Array = 545
+Python - 2 - DataTable = 545
+Python - 3 - OOP = 545
+Python - 4 - Dod = 2545
+
+
+###################
+python data science
+
+Data Science - 0 Data engineer = 545
+Data Science - 1  Data Warehouse = 545
+Data Science - 2 Data analyst = 545
+Data Science - 3 Data scientist part 1 = 545
+Data Science - 4 Data scientist part 2 = 2545
+
+
+total above = 15810
+
+
+########### 
+
+computorv1 = 4200
+ready set boole = 7000
+ft_linear_regression = 4200
+dslr = 6000
+matrix = 7000
+tweets = 9450
+
+
+hypertube = 15750
+matcha = 9450
+red-tetris = 15750
