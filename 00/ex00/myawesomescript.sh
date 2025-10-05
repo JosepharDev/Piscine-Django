41e4f5817d3099d67ce088071fdd25c796970e7c
JosepharDev <josephardev@gmail.com>
2025-10-05 11:56:44 +0100

    final

--- 00/ex00/myawesomescript.sh
+++ 00/ex00/myawesomescript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -gt 1 ]; then
-    echo "bad formate\nusage ./myawesomescript.sh  url"
+    echo "bad formate\usage ./myawesomescript.sh  url"
 elif [ $# -eq 1 ]; then
     curl -L  -s -o /dev/null -w "%{url_effective}" "$1"
 else
