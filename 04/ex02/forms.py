76eaa9551acdd0a78cdaf9c30147bd50a3d73d94
JosepharDev <josephardev@gmail.com>
2026-04-23 07:55:18 +0100

    update templates

--- 04/ex02/forms.py
+++ 04/ex02/forms.py
@@ -5,7 +5,8 @@ class TextInputForm(forms.Form):
     """Form for text input with history tracking"""
     text_input = forms.CharField(
         label='Enter your text',
-        max_length=500,
+        max_length=100,
+        min_length=5,
         widget=forms.TextInput(attrs={
             'placeholder': 'Type something here...',
             'class': 'form-input'
