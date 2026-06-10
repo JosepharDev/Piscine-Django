3091e7516bfc1c86bf0491f43bbf1192a6d1cf7e
JosepharDev <josephardev@gmail.com>
2026-06-10 10:26:25 +0100

    fix ex10

--- 05/ex10/models.py
+++ 05/ex10/models.py
@@ -1,19 +1,14 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-# Create your models here.
-
 class Planets(models.Model):
     name = models.CharField(max_length=64, unique=True, null=False)
-    climate = models.TextField(null=True, blank=True)
+    climate = models.CharField(max_length=255, null=True, blank=True)
     diameter = models.IntegerField(null=True, blank=True)
     orbital_period = models.IntegerField(null=True, blank=True)
     population = models.BigIntegerField(null=True, blank=True)
     rotation_period = models.IntegerField(null=True, blank=True)
-    surface_water = models.IntegerField(null=True, blank=True)
-    terrain = models.TextField(null=True, blank=True)
+    surface_water = models.FloatField(null=True, blank=True)
+    terrain = models.CharField(max_length=128, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -22,14 +17,16 @@ class Planets(models.Model):
 
 
 class People(models.Model):
-    name = models.CharField(max_length=64, null=False)
+    name = models.CharField(max_length=64, unique=True, null=False)
     birth_year = models.CharField(max_length=32, null=True, blank=True)
     gender = models.CharField(max_length=32, null=True, blank=True)
     eye_color = models.CharField(max_length=32, null=True, blank=True)
     hair_color = models.CharField(max_length=32, null=True, blank=True)
     height = models.IntegerField(null=True, blank=True)
-    mass = models.IntegerField(null=True, blank=True)
-    homeworld = models.ForeignKey(Planets, on_delete=models.CASCADE, to_field="name", null=True, blank=True)
+    mass = models.FloatField(null=True, blank=True)
+    homeworld = models.ForeignKey(
+        Planets, on_delete=models.CASCADE, null=True, blank=True
+    )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -38,13 +35,20 @@ class People(models.Model):
 
 
 class Movies(models.Model):
+    # This model is based on ex01, with the new ManyToManyField.
     title = models.CharField(max_length=64, unique=True, null=False)
     episode_nb = models.IntegerField(primary_key=True)
-    opening_crawl = models.TextField(null=True)
+    opening_crawl = models.TextField(null=True, blank=True)
     director = models.CharField(max_length=32, null=False)
     producer = models.CharField(max_length=128, null=False)
     release_date = models.DateField(null=False)
-    characters = models.ManyToManyField(People, related_name="movies")
+
+    # This creates a many-to-many relationship between Movies and People.
+    # A movie can have many characters, and a character can be in many movies.
+    # Django will automatically create an intermediate table in the database to manage this link.
+    # We add related_name='movies' so that we can do person.movies.all()
+    characters = models.ManyToManyField(People, related_name='movies')
+
 
     def __str__(self):
-        return  self.title
+        return self.title
